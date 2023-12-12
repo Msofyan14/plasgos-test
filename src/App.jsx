9f021bc293375c150dc989e0e21656402aae7160
Muhammad Sofyan <mxsofyan14@gmail.com>
2023-12-12 11:52:29 +0700

    fix gap each div

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 function App() {
   return (
     <>
-      <div className="max-w-5xl flex flex-col h-screen justify-center items-center">
+      <div className="max-w-5xl flex flex-col h-screen justify-center items-center mx-auto">
         <div className="flex gap-x-10">
-          <div className="flex flex-col gap-y-10 mt-[8.5rem]  -m-[0.1px]">
+          <div className="flex flex-col gap-y-10 mt-[138px] -m-[0.5px]">
             <div className="bg-[#008D06] w-24 h-24 rotate-45 "></div>
           </div>
 
-          <div className="flex flex-col gap-y-10 mt-[68px] mb-[6rem] -m-[4.1rem]">
+          <div className="flex flex-col gap-y-[42px] mt-[69px] mb-[6rem] -m-[66px]">
             {[...Array(2)].map((_, index) => (
               <div
                 key={index}
@@ -16,7 +16,7 @@ function App() {
             ))}
           </div>
 
-          <div className="flex flex-col gap-y-10">
+          <div className="flex flex-col gap-y-[42px]">
             {[...Array(3)].map((_, index) => (
               <div
                 key={index}
@@ -25,7 +25,7 @@ function App() {
             ))}
           </div>
 
-          <div className="flex flex-col gap-y-10 mt-[68px] mb-[6rem] -m-[4.1rem]">
+          <div className="flex flex-col gap-y-[42px] mt-[69px] mb-[6rem] -m-[66px]">
             {[...Array(2)].map((_, index) => (
               <div
                 key={index}
@@ -34,9 +34,14 @@ function App() {
             ))}
           </div>
 
-          <div className="flex flex-col gap-y-10 mt-[8.5rem]  -m-[0.5px]">
+          <div className="flex flex-col gap-y-10 mt-[138px]  -m-[0.5px]">
             <div className="bg-[#008D06] w-24 h-24 rotate-45 "></div>
           </div>
+        </div>
+        <div className="mt-10">
+          <h1 className="font-lobster">
+            Selamat Hari Raya Idul Fitri <br /> Mohon Maaf Lahir Dan Batin
+          </h1>
         </div>
       </div>
     </>
